86297201aee9053728b76838f200ba110bf67b2f
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-05-27 11:04:59 +0300

    Add basic testing of Commando and Weapon classes in Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Commando commando = new Commando("John Smith", "Ghost");
+        Console.WriteLine($"Commando Code Name: {commando.CodeName}");
+        Console.WriteLine($"Commando Tools: {string.Join(", ", commando.Tools)}");
+        Console.WriteLine($"Commando Status: {commando.Status}");
+
+        Weapon weapon = new Weapon("Rifle", "MilTech", 30);
+        Console.WriteLine($"Weapon: {weapon.Name} by {weapon.Manufacturer} with {weapon.BulletCount} bullets");
+    }
+}
